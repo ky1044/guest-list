799c06eb7c95c9039ebce16aa2cf5913f13ecba3
Ken Yokokawa <ky1044@users.noreply.github.com>
2023-11-24 13:44:07 -0800

    code cleanup

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,11 @@
 import * as ReactDOM from "react-dom";
 import { Layout } from "./components/Layout";
 import CredentialContainer from "./components/CredentialContainer";
-import { Example } from "./components/Example";
-import ResizableTable from "./components/ResizableTable";
 
 function render() {
   ReactDOM.render(
     <Layout>
       <CredentialContainer />
-      {/* <Example /> */}
-      {/* <ResizableTable /> */}
     </Layout>,
 
     document.body
